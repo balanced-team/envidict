2a46b6775db0b73c0cbc59dc77b48b74dc8602b5
Nguyễn Thị Thúy <thuynt99@users.noreply.github.com>
2020-03-17 21:52:01 +0700

    Format code

--- src/styles/colors.js
+++ src/styles/colors.js
@@ -1,21 +1,21 @@
-export const PRIMARY = '#1779ba';
-export const SECONDARY = '#767676';
-export const WHITE = '#FFFFFF';
-export const BLACK = '#000000';
+export const PRIMARY = '#1779ba'
+export const SECONDARY = '#767676'
+export const WHITE = '#FFFFFF'
+export const BLACK = '#000000'
 
 // ACTIONS
-export const SUCCESS = '#3adb76';
-export const WARNING = '#ffae00';
-export const ALERT = '#cc4b37';
+export const SUCCESS = '#3adb76'
+export const WARNING = '#ffae00'
+export const ALERT = '#cc4b37'
 // BLUE
-export const BLUE_DARK = '#1c74bb';
-export const BLUE_MEDIUM = '#3792d0';  // mau button
-export const BLUE_LIGHT = '#4297d3';   // vi du tieng anh
-export const BLUE_TITLE = '#003a68';
-export const BLUE_TEXT = '#044470';    // tu dang tim kiem
-export const BLUE_EXPLAN = '#01416e';  // Giai thich, nghia
-export const BLUE_TRANSLATE = '#8b8b8b';// vi du tieng viet
+export const BLUE_DARK = '#1c74bb'
+export const BLUE_MEDIUM = '#3792d0' // mau button
+export const BLUE_LIGHT = '#4297d3' // vi du tieng anh
+export const BLUE_TITLE = '#003a68'
+export const BLUE_TEXT = '#044470' // tu dang tim kiem
+export const BLUE_EXPLAN = '#01416e' // Giai thich, nghia
+export const BLUE_TRANSLATE = '#8b8b8b' // vi du tieng viet
 
 // GREEN
-export const GREEN_MEDIUM = '#00c100';
-export const GREEN_LIGHT = '#80ff80';
+export const GREEN_MEDIUM = '#00c100'
+export const GREEN_LIGHT = '#80ff80'
