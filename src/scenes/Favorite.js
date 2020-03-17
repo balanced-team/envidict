e00e424e0f1aff9af2378006d10ca4cca0aa20bd
hoangkimminh <vatliyeutoi@gmail.com>
2020-03-18 02:15:24 +0700

    Fix app don't exit after pressing hardware back button

--- src/scenes/Favorite.js
+++ src/scenes/Favorite.js
@@ -1,8 +1,14 @@
-import React from 'react'
-import {Text} from 'react-native'
+import React, { useEffect } from 'react'
+import { Text, Alert, BackHandler } from 'react-native'
 import { View } from 'native-base'
 
+import { backHandleToExitApp } from '../utils'
+
 const Favorite = () => {
+  useEffect(() => {
+    backHandleToExitApp(Alert, BackHandler)
+  }, [])
+
   return (
     <View>
       <Text>Favorite</Text>
