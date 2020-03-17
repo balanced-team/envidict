ccf910c4a60d1cf4db23aa83056df32710d6806b
Nguyễn Thị Thúy <thuynt99@users.noreply.github.com>
2020-03-17 22:00:32 +0700

    Format code

--- src/styles/colors.js
+++ src/styles/colors.js
@@ -9,12 +9,12 @@ export const WARNING = '#ffae00'
 export const ALERT = '#cc4b37'
 // BLUE
 export const BLUE_DARK = '#1c74bb'
-export const BLUE_MEDIUM = '#3792d0' // mau button
-export const BLUE_LIGHT = '#4297d3' // vi du tieng anh
+export const BLUE_MEDIUM = '#3792d0' // Mau button
+export const BLUE_LIGHT = '#4297d3' // Vi du tieng anh
 export const BLUE_TITLE = '#003a68'
-export const BLUE_TEXT = '#044470' // tu dang tim kiem
-export const BLUE_EXPLAN = '#01416e' // Giai thich, nghia
-export const BLUE_TRANSLATE = '#8b8b8b' // vi du tieng viet
+export const BLUE_TEXT = '#044470' // Tu dang tim kiem
+export const BLUE_EXPLAIN = '#01416e' // Giai thich, nghia
+export const BLUE_TRANSLATE = '#8b8b8b' // Vi du tieng viet
 
 // GREEN
 export const GREEN_MEDIUM = '#00c100'
