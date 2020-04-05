bb9c4e38ae402aac9b36d128ca84ac5829cc8141
hoangkimminh <minhhk0312.uet@gmail.com>
2020-04-05 19:28:40 +0700

    Fix

---
+++ src/api/index.js
@@ -0,0 +1,3 @@
+import * as translateOnlineAPI from './translateOnlineApi'
+
+export { translateOnlineAPI }
---
+++ src/components/atoms/online-translation.js/LanguagePicker.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import { Form, Picker } from 'native-base'
+
+const LanguagePicker = (props) => {
+  const { langs, defaultSelected, onChangeLang } = props
+  const onValueChange = (value) => {
+    onChangeLang(value)
+  }
+
+  return (
+    <Form>
+      <Picker
+        mode="dropdown"
+        style={{ width: 140 }}
+        selectedValue={defaultSelected}
+        onValueChange={onValueChange}
+      >
+        {langs.map((lang, index) => {
+          return <Picker.Item key={index} label={lang.label} value={lang.value} />
+        })}
+      </Picker>
+    </Form>
+  )
+}
+
+export default LanguagePicker
