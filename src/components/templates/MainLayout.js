d8f54f2b09f72483b476937c642f247898626b22
huonghv.adsplus <huonghv.adsplus@gmail.com>
2020-05-25 00:22:36 +0700

    Add scroll to top, dark theme

--- src/components/templates/MainLayout.js
+++ src/components/templates/MainLayout.js
@@ -1,21 +1,23 @@
 import React from 'react'
 import { Container, Content } from 'native-base'
 import { ScrollView } from 'react-native'
+import { useScrollToTop } from '@react-navigation/native'
 
 import SearchHeader from '../molecules/main-layout/SearchHeader'
 
 const MainLayout = (props) => {
-  const { onGoToSearchView, autoFocusSearchInput, isVocabularySearch } = props
+  const { onGoToSearchView, autoFocusSearchInput } = props
+  const ref = React.useRef(null)
+  useScrollToTop(ref)
   return (
     <Container>
       <SearchHeader
         autoFocus={autoFocusSearchInput}
         onGoToSearchView={onGoToSearchView}
         voiceButtonIsVisible={props.voiceButtonIsVisible}
-        isVocabularySearch={isVocabularySearch}
       />
       <Content>
-        <ScrollView>{props.children}</ScrollView>
+        <ScrollView ref={ref}>{props.children}</ScrollView>
       </Content>
     </Container>
   )
